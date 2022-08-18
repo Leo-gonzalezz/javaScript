f8b454f6d0b6059de2bb5f80a09c1aad74704ca9
leonel gonzalez <leogonzalez_2752@hotmail.com>
2022-08-17 21:17:53 -0300

    desafio-eventos

--- scrips/stock.js
+++ scrips/stock.js
@@ -1,21 +1,22 @@
+//stock de productos//
 let stockProductos = [
     {id: 1, nombre: "amoladora bosch", tipo: "amoladora", desc: "angular 450w", precio: 8999, marca: "bosch", img: './img/amoladora-bosch.jpeg'},
     {id: 2, nombre: "amoladora stanley", tipo: "amoladora", desc: "angular 550w", precio: 11900, marca: "stanley", img: './img/amoladora-stanley.jpg'},
     {id: 3, nombre: "amoladora dewalt", tipo: "amoladora", desc: "angular + esmeril 600w", precio: 12000, marca: "dewalt", img: './img/amoladora-dewalt.jpg'},
-    {id: 4, nombre: "taladro dewalt", tipo: "taladro", desc: "560w percutor", precio: 23000, marca: "dewalt", img: '/img/remera.jpeg'},
-    {id: 5, nombre: "atornilladora bosch", tipo: "atornilladora", desc: "12 v/2 baterias", precio: 33888, marca: "bosch", img: '/img/remera.jpeg'},
-    {id: 6, nombre: "termo stanley", tipo: "termo", desc: "acero inoxidable ", precio: 12999, marca: "stanley", img: '/img/remera.jpeg'},
-    {id: 7, nombre: "hidrolavadora dewalt", tipo: "hidrolavadora", desc: "1000 bar", precio: 29000, marca: "dewalt", img: '/img/remera.jpeg'},
-    {id: 8, nombre: "ingletadora bosch", tipo: "ingletadora", desc: "1300w", precio: 34699, marca: "bosch", img: '/img/remera.jpeg'},
-    {id: 9, nombre: "sierra circular stanley", tipo: "sierra circular", desc: "1200w ", precio: 30000, marca: "stanley", img: '/img/remera.jpeg'},
-    {id: 10, nombre: "atornilladora stanley", tipo: "aspiradora", desc: "860w ", precio: 22999, marca: "stanley", img: '/img/camisa.jpeg'},
-    {id: 11, nombre: "aspiradora dewalt", tipo: "aspiradora", desc: "interior/exterior 1000w", precio: 12300, marca: "dewalt", img: '/img/camisa.jpeg'},
-    {id: 12, nombre: "rotomartillo bosch", tipo: "rotomartillo", desc: "1000w", precio: 50999, marca: "bosch", img: '/img/camisa.jpeg'},
-    {id: 13, nombre: "ingletadora dewealt", tipo: "ingletadora", desc: "1200w", precio: 23999, marca: "dewalt", img: '/img/camisa.jpeg'},
-    {id: 14, nombre: "sierra circular bosch", tipo: "sierra circular", desc: "7 1/4 990w", precio: 45888, marca: "bosch", img: '/img/camisa.jpeg'},
-    {id: 15, nombre: "taladro stanley", tipo: "taladro", desc: "660w percutor", precio: 24000, marca: "stanley", img: '/img/camisa.jpeg'},
-    {id: 16, nombre: "atornilladora dewalt", tipo: "atornilladora", desc: "20 v/2 baterias", precio: 25999, marca: "dewalt", img: '/img/pantalon.jpeg'},
-
+    {id: 4, nombre: "taladro dewalt", tipo: "taladro", desc: "560w percutor", precio: 23000, marca: "dewalt", img: './img/taladro-dewalt.jpg'},
+    {id: 5, nombre: "atornilladora bosch", tipo: "atornilladora", desc: "12 v/2 baterias", precio: 33888, marca: "bosch", img: './img/atornilladora-bosch.jpg'},
+    {id: 6, nombre: "termo stanley", tipo: "termo", desc: "acero inoxidable ", precio: 12999, marca: "stanley", img: './img/termo-stanley.jpg'},
+    {id: 7, nombre: "hidrolavadora dewalt", tipo: "hidrolavadora", desc: "1000 bar", precio: 29000, marca: "dewalt", img: './img/hidrolavadora-dewalt.jpg'},
+    {id: 8, nombre: "ingletadora bosch", tipo: "ingletadora", desc: "1300w", precio: 34699, marca: "bosch", img: './img/ingletadora-bosch.jpg'},
+    {id: 9, nombre: "sierra circular stanley", tipo: "sierra circular", desc: "1200w ", precio: 30000, marca: "stanley", img: './img/sierra circular-stanley.webp'},
+    {id: 10, nombre: "atornilladora stanley", tipo: "aspiradora", desc: "860w ", precio: 22999, marca: "stanley", img: './img/aspiradora-stanley.jpg'},
+    {id: 11, nombre: "aspiradora dewalt", tipo: "aspiradora", desc: "interior/exterior 1000w", precio: 12300, marca: "dewalt", img: './img/aspiradora -dewalt.jpg'},
+    {id: 12, nombre: "rotomartillo bosch", tipo: "rotomartillo", desc: "1000w", precio: 50999, marca: "bosch", img: './img/rotomartillo-bosch.jpg'},
+    {id: 13, nombre: "ingletadora dewalt", tipo: "ingletadora", desc: "1200w", precio: 23999, marca: "dewalt", img: './img/ingletadora-dewalt.jpg'},
+    {id: 14, nombre: "sierra circular bosch", tipo: "sierra circular", desc: "7 1/4 990w", precio: 45888, marca: "bosch", img: './img/sierra circular-bosch.jpg'},
+    {id: 15, nombre: "taladro stanley", tipo: "taladro", desc: "660w percutor", precio: 24000, marca: "stanley", img: './img/taladro-stanley.png'},
+    {id: 16, nombre: "atornilladora dewalt", tipo: "atornilladora", desc: "20 v/2 baterias", precio: 25999, marca: "dewalt", img: './img/atornilladora-dewalt.jpg'},
+    {id: 17, nombre: "caladora bosch", tipo: "caladora", desc: "720w", precio: 11799, marca: "bosch", img: './img/caladora-bosch.png'},
 ]
 
 
